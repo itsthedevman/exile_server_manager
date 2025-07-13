a94acf1d05b511b386c0ae542ed37336a2c6669e
Bryan <bryan@itsthedevman.com>
2025-07-12 22:24:29 -0400

    Initial commit

---
+++ core/lib/esm_ruby_core.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "esm_ruby_core/version"
+
+module EsmRubyCore
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ core/lib/esm_ruby_core/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module EsmRubyCore
+  VERSION = "0.1.0"
+end
