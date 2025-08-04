1f066de133191ed2388de783fde4e14fff0102ce
Bryan <bryan@itsthedevman.com>
2025-08-04 16:01:43 -0400

    Import migrations

---
+++ core/db/migrate/20220203013142_add_access_roles_to_community.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddAccessRolesToCommunity < ActiveRecord::Migration[6.1]
+  def change
+    add_column(:communities, :dashboard_access_role_ids, :json, default: [])
+  end
+end
