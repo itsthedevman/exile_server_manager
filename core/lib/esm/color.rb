95548cc52073c364b713e0f86a02bd94c3f611ae
Bryan <bryan@itsthedevman.com>
2025-08-04 01:10:00 -0400

    Refactor color definitions to group colors in ALL arrays for better organization

--- core/lib/esm/color.rb
+++ core/lib/esm/color.rb
@@ -2,36 +2,36 @@
 
 module ESM
   module Color
-    BLUE = "#1E354D"
-    RED = "#CE2D4E"
+    ALL = [
+      BLUE = "#1E354D",
+      RED = "#CE2D4E"
+    ].freeze
 
     module Toast
-      RED = "#C62551"
-      BLUE = "#3ED3FB"
-      GREEN = "#9FDE3A"
-      YELLOW = "#DECA39"
-      ORANGE = "#C64A25"
-      BURNT_ORANGE = "#7D2F00"
-      PURPLE = "#793ADE"
-      LAVENDER = "#344D71"
-      PINK = "#DE3A9F"
-      WHITE = "#FFFFFF"
-      STEEL_GREEN = "#2F4858"
-      BROWN = "#574143"
-      SAGE = "#E9F6D0"
+      ALL = [
+        RED = "#C62551",
+        BLUE = "#3ED3FB",
+        GREEN = "#9FDE3A",
+        YELLOW = "#DECA39",
+        ORANGE = "#C64A25",
+        BURNT_ORANGE = "#7D2F00",
+        PURPLE = "#793ADE",
+        LAVENDER = "#344D71",
+        PINK = "#DE3A9F",
+        WHITE = "#FFFFFF",
+        STEEL_GREEN = "#2F4858",
+        BROWN = "#574143",
+        SAGE = "#E9F6D0"
+      ].freeze
 
-      def self.colors
-        constants(false).map { |c| const_get(c) }.select { |c| c.is_a?(String) }
+      def self.to_h
+        (constants(false) - [:ALL]).to_h { |c| [c.downcase, const_get(c)] }
       end
-    end
-
-    def self.colors
-      constants(false).map { |c| const_get(c) }.select { |c| c.is_a?(String) }
     end
 
     # Randomly select a color from the full color pool
     def self.random
-      (colors + ESM::Color::Toast.colors).sample(1).first
+      (ALL + ESM::Color::Toast::ALL).sample.first
     end
   end
 end
