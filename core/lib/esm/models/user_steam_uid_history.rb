13df5adf95143f145932188df063764c4ae0deca
Bryan <bryan@itsthedevman.com>
2025-08-11 23:04:22 -0400

    Rename UserSteamUidHistory class to UserSteamUIDHistory for consistency

--- core/lib/esm/models/user_steam_uid_history.rb
+++ core/lib/esm/models/user_steam_uid_history.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ESM
-  class UserSteamUidHistory < ApplicationRecord
+  class UserSteamUIDHistory < ApplicationRecord
     # =============================================================================
     # INITIALIZE
     # =============================================================================
