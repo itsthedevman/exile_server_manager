215d8a614fbcacdd18cc9f792d928183aa80f00f
Bryan <bryan@itsthedevman.com>
2025-08-24 22:41:54 -0400

    refactor(server): Rename scope for server ID lookup to improve clarity

--- core/lib/esm/models/server.rb
+++ core/lib/esm/models/server.rb
@@ -78,7 +78,7 @@ module ESM
     end
 
     def self.find_by_server_id(id)
-      includes(:community).matching_server_id(id).first
+      includes(:community).with_server_id(id).first
     end
 
     # =============================================================================
