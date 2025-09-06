7a318332817b98127a75a503ade87c0887103ec8
Bryan <bryan@itsthedevman.com>
2025-09-06 16:23:24 -0400

    feat: Add years_alive calculation and update index view for uptime display

--- website/app/controllers/home_controller.rb
+++ website/app/controllers/home_controller.rb
@@ -3,6 +3,7 @@
 class HomeController < ApplicationController
   def index
     command_count = ESM::CommandDetail.all.size
-    render locals: {command_count:}
+    years_alive = Date.today.year - 2018
+    render locals: {command_count:, years_alive:}
   end
 end
