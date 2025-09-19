3feab8ea48f419df0c57f4eae19626a0027dde2d
Bryan <bryan@itsthedevman.com>
2025-09-19 19:00:39 -0400

    feat: Update DiscordNotifier to use logging webhook URL from settings configuration

--- website/app/models/exception_notifier/discord_notifier.rb
+++ website/app/models/exception_notifier/discord_notifier.rb
@@ -9,7 +9,7 @@ module ExceptionNotifier
     def call(exception, options = {})
       backtrace = Rails.backtrace_cleaner.clean(exception.backtrace[0..10]).join("\n\t")
       HTTParty.post(
-        "https://discord.com/api/webhooks/1067235053307973633/IaH7FWosk3nYj0YOd3-T9-4Pu1wwLkS1pLfMsbNocI3XpMN8bZjgvmmlEbyH3imu4Q3H",
+        Settings.discord.logging_webhook_url,
         body: {
           content: <<~STRING
             `#{Time.current}`
