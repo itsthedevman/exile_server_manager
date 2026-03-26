4b50a98415e14eddf3e938a0a25d857405ddee09
Bryan <bryan@itsthedevman.com>
2026-03-25 21:06:39 -0400

    Ruby 3.4

--- core/flake.nix
+++ core/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Ruby 3.3 development environment";
+  description = "Ruby 3.4 development environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -20,10 +20,7 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            (ruby_3_3.override {
-              jemallocSupport = false;
-              docSupport = false;
-            })
+            ruby_3_4
 
             # Dependencies for native gems
             pkg-config
