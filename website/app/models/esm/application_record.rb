3f24b41614df72e0aefc71b1340969b5d3c67761
Bryan <bryan@itsthedevman.com>
2026-05-17 19:27:49 -0400

    website: move dom_id/to_param onto ESM::ApplicationRecord

--- website/app/models/esm/application_record.rb
+++ website/app/models/esm/application_record.rb
@@ -3,5 +3,13 @@
 module ESM
   class ApplicationRecord < ActiveRecord::Base
     include PublicAttributes
+
+    def dom_id
+      "#{self.class.table_name.singularize}-#{public_id}"
+    end
+
+    def to_param
+      public_id
+    end
   end
 end
