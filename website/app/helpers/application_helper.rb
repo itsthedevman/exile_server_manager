eaaffb20fbe24492a57006ac176dc4814b619be7
Bryan <bryan@itsthedevman.com>
2026-05-31 02:03:58 -0400

    website: added poptab icon

--- website/app/helpers/application_helper.rb
+++ website/app/helpers/application_helper.rb
@@ -17,6 +17,17 @@ module ApplicationHelper
     Digest::SHA256.hexdigest(data)[0..24]
   end
 
+  def poptabs(amount)
+    url = image_path("poptab.png")
+
+    icon = content_tag(:span, "",
+      class: "poptab-icon ms-2 mb-2 align-text-bottom",
+      role: "img", "aria-label": "poptabs",
+      style: "mask-image: url(#{url}); -webkit-mask-image: url(#{url});")
+
+    safe_join([number_with_delimiter(amount), icon])
+  end
+
   def nav_spacer
     safe_join([
       content_tag(:hr, nil, class: "hr.d-block.d-lg-none"),
