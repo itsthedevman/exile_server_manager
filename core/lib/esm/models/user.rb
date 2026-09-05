88bac3ecbf421af97cbd435f7dee8bc826d7e873
Bryan <bryan@itsthedevman.com>
2026-09-05 01:26:01 -0400

    core: fix has_one to has_many

--- core/lib/esm/models/user.rb
+++ core/lib/esm/models/user.rb
@@ -61,7 +61,7 @@ module ESM
     has_many :user_steam_uid_history, dependent: :nullify
     has_one :user_steam_data, -> { order(:user_id) }, dependent: :destroy
 
-    has_one :server_reward_claim, dependent: :destroy
+    has_many :server_reward_claims, dependent: :destroy
 
     # =============================================================================
     # VALIDATIONS
